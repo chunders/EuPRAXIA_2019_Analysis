3acd3f52a52069b0f99982580ca18cec4151f12d
Christopher Underwood <christopher.underwood@york.ac.uk>
2019-11-18 20:50:29 +0100

    Init commit of Wavefront_sensor/phasics.py

---
+++ Wavefront_sensor/phasics.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""    _ 
+      /  |     | __  _ __  _
+     /   |    /  |_||_|| ||
+    /    |   /   |  |\ | ||_
+   /____ |__/\ . |  | \|_|\_|
+   __________________________ .
+   
+Created on Mon Nov 18 17:17:36 2019
+
+@author: chrisunderwood
+    Analysis of the wavefront sensor
+"""
+import numpy as np
+import matplotlib as mpl
+mpl.rcParams['figure.figsize'] = [8.0,6.0]
+import matplotlib.pyplot as plt
+
+# Load my module of functions
+import sys
+sys.path.insert(0, '/Users/chrisunderwood/Documents/Python/')
+import CUnderwood_Functions3 as func
+
+class wfs_zernlike():
+    def __init__(self, filepath):
+        pass
+        
+        
+if __name__ == "__main__":    
+    path_to_data = "/Volumes/Lund_York/"
+    date = "2019-11-15/"
+    run = "0001/"
+    diagnostic = "Lanex/"
+
