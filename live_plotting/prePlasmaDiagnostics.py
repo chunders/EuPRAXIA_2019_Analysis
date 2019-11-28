8ef437a12b628423a6e7fcb12ec7d54c6414ad7e
iclaserplasma <42937440+iclaserplasma@users.noreply.github.com>
2019-11-28 13:10:26 +0100

    Added live plotting stuff

--- live_plotting/prePlasmaDiagnostics.py
+++ live_plotting/prePlasmaDiagnostics.py
@@ -9,9 +9,3 @@ def img2spec(filePath,calFile= r'Z:\2019 EuPRAXIA\OrielMysteryGrating_lambda.mat
     specCal = loadmat(calFile)
     return specCal['lambdaAxis'].flatten(), np.mean(img.astype(float),axis=0)
 
-def getSpectra(filePaths,calFile):
-    S_l_list = []
-    for f in filePaths:
-        l,S_l = img2spec(f,calFile= calFile)
-        S_l_list.append(S_l)
-    return l,S_l_list
